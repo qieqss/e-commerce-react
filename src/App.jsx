69ccb65a674edff18c45627362a2b17c733bbfff
Alexey <kulakovalexey@proton.me>
2022-08-17 10:58:01 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { books } from "./data";
